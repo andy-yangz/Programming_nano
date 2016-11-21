eaca18ba043938c7180d8467b82eec0e80967969
Andy <andy.yangzhen@gmail.com>
2016-11-22 00:34:31 +0900

    add movie informations

---
+++ stage3/Project/entertainment_center.py
@@ -0,0 +1,43 @@
+# Lesson 3.4: Make Classes
+# Mini-Project: Movies Website
+
+# In this file, you will define instances of the class Movie defined
+# in media.py. After you follow along with Kunal, make some instances
+# of your own!
+
+# After you run this code, open the file fresh_tomatoes.html to
+# see your webpage!
+
+import media
+import fresh_tomatoes
+
+	
+the_shawshank_redemption = media.Movie("The Shawshank Redemption",
+							"https://img3.doubanio.com/view/photo/photo/public/p480747492.jpg",
+							"https://www.youtube.com/watch?v=6hB3S9bIaco",
+							"Frank Darabont",
+							"Frank Darabont, Stephen King",
+							"Tim Robbins, Morgan Freeman",
+							"142 min",
+							'1994-10-14')
+
+life_is_beautiful = media.Movie("La vita e bella",
+							"https://img3.doubanio.com/view/photo/photo/public/p2194576972.jpg",
+							"https://www.youtube.com/watch?v=pysuUJhOnv4",
+							"Roberto Benigni",
+							"Roberto Benigni, Vincenzo Cerami",
+							"Roberto Benigni, Nicoletta Braschi, VGiorgio Cantarini",
+							"116 min",
+							"1997-12-20")
+
+farewell_my_concubine = media.Movie("Farewell My Concubine",
+							"https://img1.doubanio.com/view/photo/photo/public/p2390676609.jpg",
+							"https://www.youtube.com/watch?v=6qvMdtFmcjQ",
+							"Kaige Chen",
+							"Wei Lu, Lilian Lee Pik-Wah",
+							"Leslie Cheung, Fengyi Zhang, Li Gong",
+							"171 min",
+							"1993-01-01")	
+
+movies = [the_shawshank_redemption, life_is_beautiful, farewell_my_concubine]
+fresh_tomatoes.open_movies_page(movies)
--- stage3/Project/fresh_tomatoes.py
+++ stage3/Project/fresh_tomatoes.py
@@ -1,4 +1,11 @@
+import webbrowser
+import os
+import re
 
+# Styles and scripting for the page
+main_page_head = '''
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <title>Fresh Tomatoes!</title>
@@ -30,7 +37,6 @@
         .movie-tile {
             margin-bottom: 20px;
             padding-top: 20px;
-            text-align: left;
         }
         .movie-tile:hover {
             background-color: #EEE;
@@ -48,6 +54,13 @@
             left: 0;
             top: 0;
             background-color: white;
+        }
+        .info {
+            text-align: left;
+            padding-left: 60px;
+        }
+        .pl {
+            font-weight: bold;
         }
     </style>
     <script type="text/javascript" charset="utf-8">
@@ -76,15 +89,16 @@
         });
     </script>
 </head>
+'''
 
-<!DOCTYPE html>
-<html lang="en">
+# The main page layout and title bar
+main_page_content = '''
   <body>
     <!-- Trailer Video Modal -->
     <div class="modal" id="trailer">
       <div class="modal-dialog">
         <div class="modal-content">
-          <a href="#" class="hanging-close" data-dismiss="modal" aria-hidden="ture">
+          <a href="#" class="hanging-close" data-dismiss="modal" aria-hidden="true">
             <img src="https://lh5.ggpht.com/v4-628SilF0HtHuHdu5EzxD7WRqOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_VJoF7RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24"/>
           </a>
           <div class="scale-media" id="trailer-video-container">
@@ -98,23 +112,66 @@
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">Andy's Favorite Movie Trailers</a>
           </div>
         </div>
       </div>
     </div>
     <div class="container">
-      
-<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="KYz2wyBy3kc" data-toggle="modal" data-target="#trailer">
-    <img src="http://www.gstatic.com/tv/thumb/movieposters/17420/p17420_p_v8_ab.jpg" width="220" height="342">
-    <h2>Toy Story</h2>
-</div>
-
-<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="owK1qxDselE" data-toggle="modal" data-target="#trailer">
-    <img src="https://upload.wikimedia.org/wikipedia/en/8/8d/Gladiator_ver1.jpg" width="220" height="342">
-    <h2>Gladiator</h2>
-</div>
-
+      {movie_tiles}
     </div>
   </body>
 </html>
+'''
+
+# A single movie entry html template
+movie_tile_content = '''
+<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
+    <img src="{poster_image_url}" width="220" height="342">
+    <h2>{movie_title}</h2>
+    <div class="info" >
+        <span class="pl">Director</span>: {director} <br>
+        <span class="pl">Screen Writer</span>: {screen_writer} <br>
+        <span class="pl">Stars</span>: {stars} <br>
+        <span class="pl">Length</span>: {length} <br>
+        <span class="pl">Release Date</span>: {release_date} <br>
+    </div>
+</div>
+'''
+
+def create_movie_tiles_content(movies):
+    # The HTML content for this section of the page
+    content = ''
+    for movie in movies:
+        # Extract the youtube ID from the url
+        youtube_id_match = re.search(r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
+        youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
+        trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
+
+        # Append the tile for the movie with its content filled in
+        content += movie_tile_content.format(
+            movie_title=movie.title,
+            poster_image_url=movie.poster_image_url,
+            trailer_youtube_id=trailer_youtube_id,
+            director = movie.director,
+            screen_writer = movie.screen_writer,
+            stars = movie.stars,
+            length = movie.length,
+            release_date = movie.release_date
+        )
+    return content
+
+def open_movies_page(movies):
+  # Create or overwrite the output file
+  output_file = open('fresh_tomatoes.html', 'w')
+
+  # Replace the placeholder for the movie tiles with the actual dynamically generated content
+  rendered_content = main_page_content.format(movie_tiles=create_movie_tiles_content(movies))
+
+  # Output the file
+  output_file.write(main_page_head + rendered_content)
+  output_file.close()
+
+  # open the output file in the browser
+  url = os.path.abspath(output_file.name)
+  webbrowser.open('file://' + url, new=2) # open in a new tab, if possible
